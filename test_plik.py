447f0638e42ad406932bd18a7c179abad569494c
Remigiusz Kamiński <50remek50@gmail.com>
2022-01-30 19:18:04 +0100

    er

--- test_plik.py
+++ test_plik.py
@@ -7,8 +7,3 @@ def test_add():
     assert plik.add(3.9, 4) == 7
     assert plik.add(3.9, 4.1) == 8
 
-
-def test_to_sentence():
-    assert plik.to_sentence('apple') == 'Apple.'
-    assert plik.to_sentence('Apple trees') == 'Apple trees.'
-    assert plik.to_sentence('Apple trees.') == 'Apple trees.'
